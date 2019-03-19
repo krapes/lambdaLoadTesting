575396eb044f74f9b9442261d198d9bbb95744ef
kerrirapes <kerri.rapes@conversica.com>
2019-03-19 13:02:30 -0300

    Feature: make run.sh executable

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 AWS_ACCESS_KEY_ID=$(aws --profile honestAbe configure get aws_access_key_id)
 AWS_SECRET_ACCESS_KEY=$(aws --profile honestAbe configure get aws_secret_access_key)
+AWS_REGION=us-east-1
 
 
 
@@ -10,4 +11,4 @@ docker build -t concurrency:latest  .
 docker run -it --rm \
    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-   -e AWS_REGION=us-east-1 concurrency:latest
+   -e AWS_REGION=$AWS_REGION concurrency:latest
