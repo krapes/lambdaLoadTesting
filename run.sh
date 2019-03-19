de722d50b5deac77e9d77df267d3e4b1f728f108
kerrirapes <kerri.rapes@conversica.com>
2019-03-19 17:16:20 -0300

    Fix: add if statements to allow aws creds from travis

--- run.sh
+++ run.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
-AWS_ACCESS_KEY_ID=$(aws --profile honestAbe configure get aws_access_key_id)
-AWS_SECRET_ACCESS_KEY=$(aws --profile honestAbe configure get aws_secret_access_key)
+
+if [[ -z "${aws_access_key_id}" ]]; then
+  AWS_ACCESS_KEY_ID=$(aws --profile honestAbe configure get aws_access_key_id)
+else
+  AWS_ACCESS_KEY_ID="${aws_access_key_id}"
+fi
+
+#AWS_ACCESS_KEY_ID=$(aws --profile honestAbe configure get aws_access_key_id)
+
+if [[ -z "${aws_secret_access_key}" ]]; then
+  AWS_SECRET_ACCESS_KEY=$(aws --profile honestAbe configure get aws_secret_access_key)
+else
+  AWS_ACCESS_KEY_ID="${aws_secret_access_key}"
+fi
+
+#AWS_SECRET_ACCESS_KEY=$(aws --profile honestAbe configure get aws_secret_access_key)
 AWS_REGION=us-east-1
 
 
